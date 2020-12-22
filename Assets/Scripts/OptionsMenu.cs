332b9904eb2f247bdfdaac14793b97a9f0b7259c
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2020-12-23 00:08:24 +0100

    Added volume slider and graphics quality dropdown to options menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
     {
-        
+        audioMixer.SetFloat("volume", volume);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetQuality(int qualityIndex)
     {
-        
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
