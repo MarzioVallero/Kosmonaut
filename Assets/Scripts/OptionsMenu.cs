ce5cbb6b1e5a7e39454c6a00cb26835e2a88bbd1
Gianluca Garganese <twh1897@gmail.com>
2021-02-14 00:28:03 +0100

    Added first video experiments, added possibility to pass options through scenes (still WIP)

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,16 +2,34 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
 
+    bool enableVibration;
     Resolution[] resolutions;
+    GameObject indestructable;
+    SettingsManager settingsManager;
 
     private void Start()
     {
+        //Prova variabile locale invece dello script esterno (forse lo script non muore nel cambio scena)
+        Debug.Log("start");
+
+        SceneManager.activeSceneChanged += ImportSettings;
+
+        indestructable = GameObject.Find("IndestructableData");
+        settingsManager = indestructable.GetComponent<SettingsManager>();
+
+        GameObject vibrationToggle = GameObject.Find("VibrationToggle");
+        Toggle toggle = vibrationToggle.GetComponent<Toggle>();
+
+        toggle.isOn = settingsManager.enableVibration;
+
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
@@ -31,6 +49,13 @@ public class OptionsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+    }
+
+    private void ImportSettings(Scene current, Scene next)
+    {
+        Debug.Log("importsettings "+settingsManager.enableVibration);
+        
+        SetVibration(settingsManager.enableVibration);
     }
 
     public void SetResolution(int resolutionIndex)
@@ -56,10 +81,18 @@ public class OptionsMenu : MonoBehaviour
 
     public void SetVibration(bool enableVibration)
     {
-        GameObject soyuz = GameObject.Find("Soyuz");
-        JoystickController joystickController = soyuz.GetComponent<JoystickController>();
+        try
+        {
+            GameObject soyuz = GameObject.Find("Soyuz");
+            JoystickController joystickController = soyuz.GetComponent<JoystickController>();
 
-        joystickController.SetVibration(enableVibration);
+            joystickController.SetVibration(enableVibration);
+        }
+        catch
+        {
+            settingsManager.enableVibration = enableVibration;
+        }
+        
     }
 }
 
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public bool enableVibration = true;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
