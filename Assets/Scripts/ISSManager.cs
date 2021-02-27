0520a90d8b3055e6696ff0495a0ff55393cf43da
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-02-27 13:36:47 +0100

    Fixed explosion null reference exception

--- Assets/Scripts/ISSManager.cs
+++ Assets/Scripts/ISSManager.cs
@@ -50,8 +50,15 @@ public class ISSManager : MonoBehaviour
             if (child.gameObject.TryGetComponent<Rigidbody>(out Rigidbody component) == true)
             {
                 child.GetComponent<Rigidbody>().isKinematic = false;
-                if (child.GetComponent<Collider>().GetType() == typeof(MeshCollider))
-                    Destroy(child.gameObject.GetComponent<Collider>());
+                try
+                {
+                    if (child.GetComponent<Collider>().GetType() == typeof(MeshCollider))
+                        Destroy(child.gameObject.GetComponent<Collider>());
+                }
+                catch
+                {
+                    Debug.Log("Exception");
+                }
                 child.gameObject.AddComponent<BoxCollider>();
                 child.GetComponent<Rigidbody>().AddExplosionForce(1f, ISS.position, 10f);
             }
